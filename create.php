77fb4bab26ae7c282c6b87711bb34d1fb54ef900
olamilekan <olamilekanomosanya@gmail.com>
2021-12-02 12:03:10 +0100

    add params to my db statement

--- create.php
+++ create.php
@@ -3,6 +3,21 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
+//saving the inputs in the database
+$title = $_POST['title'];
+$description = $_POST['description'];
+$price = $_POST['price'];
+$date = date('Y-m-d H:i:s');
+// $title = $_POST['title'];
+$statement =$pdo->prepare("INSERT INTO products (title,  image, description, price, create_date)
+            VALUES (:title, :image, :description, :price, :date)");
+
+$statement->bindValue(':title', $title);
+$statement->bindValue(':image', $image);
+$statement->bindValue(':description', $description);
+$statement->bindValue(':price', $price);
+$statement->bindValue(':date', $date);
+$statement->execute();
 
 
 
@@ -25,23 +40,23 @@ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 <body>
     <h1>CREATE NEW PRODUCTS</h1>
 
-    <form>
+    <form action="create.php" method="post">
   
   <div class="mb-3">
     <label> Product Title </label>
-    <input type="text" class="form-control" >
+    <input type="text" class="form-control" name="title">
   </div>
   <div class="mb-3">
     <label> Product Description </label>
-    <textarea class="form-control" > </textarea>
+    <textarea name="description" class="form-control" > </textarea>
   </div>
   <div class="mb-3">
     <label> Product Price </label>
-    <input type="number" step=".01" class="form-control" >
+    <input type="number" step=".01" class="form-control" name="price" >
   </div>
   <div class="mb-3">
     <label> Product Image </label> <br>
-    <input type="file" >
+    <input type="file"  name="image">
   </div>
   
   <button type="submit" class="btn btn-primary">Submit</button>
