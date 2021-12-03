c0c13dd46967671b349c4b44539a95da2f2c78be
olamilekan <olamilekanomosanya@gmail.com>
2021-12-03 16:07:34 +0100

    add images to my database

--- create.php
+++ create.php
@@ -3,24 +3,86 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-//saving the inputs in the database
+echo '<pre>';
+var_dump($_FILES);
+echo '</pre>';
+
+$errors = [];
+
+$title = '';
+$description = '';
+$price= '';
+
+
+//collecting the inputs from the user and saving it in the database
+if($_SERVER['REQUEST_METHOD'] ==='POST'){ 
 $title = $_POST['title'];
 $description = $_POST['description'];
 $price = $_POST['price'];
 $date = date('Y-m-d H:i:s');
-// $title = $_POST['title'];
+
+
+
+if(!$title){
+    $errors[]= 'product title is required';
+}
+
+if(!$description){
+    $errors[]= 'product description is required';
+}
+if(!$price){
+    $errors[]= 'product price is required';
+}
+
+
+//check if image dirctory is created
+if (!is_dir('images'))
+{
+    mkdir('images');
+}
+
+
+if(empty($errors)) {
+
+$image = $_FILES['image'] ?? null;
+$imagePath ='';
+if ($image) {
+    $imagePath = 'images/'.randomString(8).'/'.$image['name'];
+    mkdir(dirname($imagePath));
+
+
+    move_uploaded_file($image['tmp_name'],  $imagePath);
+
+}
+
 $statement =$pdo->prepare("INSERT INTO products (title,  image, description, price, create_date)
             VALUES (:title, :image, :description, :price, :date)");
 
 $statement->bindValue(':title', $title);
-$statement->bindValue(':image', $image);
+$statement->bindValue(':image', $imagePath);
 $statement->bindValue(':description', $description);
 $statement->bindValue(':price', $price);
 $statement->bindValue(':date', $date);
 $statement->execute();
+header('Location: index.php')
+}
+
+}
 
 
+// function to create random string
+function randomString($n)
+{
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $str = '';
 
+    for ($i=0; $i < $n; $i++) { 
+        $index = rand(0, strlen($characters)-1);
+        $str.= $characters[$index];
+    }
+
+    return $str;
+}
 ?>
 
 
@@ -40,19 +102,31 @@ $statement->execute();
 <body>
     <h1>CREATE NEW PRODUCTS</h1>
 
-    <form action="create.php" method="post">
+    <?php if (!empty($errors)):     ?>
+
+
+    <div class="alert alert-danger">
+    <?php  
+
+    foreach ($errors as $error): ?>
+    <div><?php echo $error   ?></div>
+
+    <?php endforeach ;  ?>
+    </div>
+<?php endif; ?>
+    <form action="create.php" method="post" enctype="multipart/form-data">
   
   <div class="mb-3">
     <label> Product Title </label>
-    <input type="text" class="form-control" name="title">
+    <input type="text" class="form-control" name="title" value="<?php echo $title ;?>">
   </div>
   <div class="mb-3">
     <label> Product Description </label>
-    <textarea name="description" class="form-control" > </textarea>
+    <textarea name="description" class="form-control"  required value="<?php echo $description ;?>"> </textarea>
   </div>
   <div class="mb-3">
     <label> Product Price </label>
-    <input type="number" step=".01" class="form-control" name="price" >
+    <input type="number" step=".01" class="form-control" name="price" value="<?php echo $price ;?>" >
   </div>
   <div class="mb-3">
     <label> Product Image </label> <br>
