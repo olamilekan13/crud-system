a569cfc61baf0c76b659bafdfb986b4cad584e7d
olamilekan <olamilekanomosanya@gmail.com>
2021-12-29 20:59:13 +0100

    add search form to search for title

--- index.php
+++ index.php
@@ -4,7 +4,23 @@ $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', ''
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 
-$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+$search = $_GET['search'] ?? '';
+if ($search){
+
+    $statement = $pdo->prepare('SELECT * FROM products WHERE title LIKE :title ORDER BY create_date DESC');
+    $statement->bindValue(':title', "%$search%");
+    
+
+}else {
+    
+
+    $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+   
+
+}
+//$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+
+// $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 // echo '<pre>';
